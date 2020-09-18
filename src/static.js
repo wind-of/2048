38795503dfbffec08433148fe18e2eb71c450d4e
wind-of <adequation@mail.ru>
2020-09-18 07:39:54 +0300

    added 11-level color

--- src/static.js
+++ src/static.js
@@ -18,4 +18,4 @@ export const moves = [
     character: "←"
   }
 ]
-export const backgroundTypes = ["", "#00d2d3", "#54a0ff", "#5f27cd", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1", "#c8d6e5", "#576574", "#222f3e"]
+export const backgroundTypes = ["", "#00d2d3", "#54a0ff", "#5f27cd", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1", "#c8d6e5", "#8395a7", "#576574", "#222f3e"]
